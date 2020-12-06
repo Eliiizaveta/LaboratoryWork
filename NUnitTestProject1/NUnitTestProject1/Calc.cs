49edf619b5ee6e1584d0f15944b1223ea368c029
Elizaveta <lizavetaop@mail.ru>
2020-12-06 12:06:09 +0300

    Lab 6 completed(tests)

---
+++ NUnitTestProject1/NUnitTestProject1/Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject1
+{
+   public static class Calc
+    {
+        static public int Plus(int x,int y)
+        {
+            return x + y;
+        }
+        static public int Minus(int x, int y)
+        {
+            return x - y;
+        }
+        static public int Mult(int x, int y)
+        {
+            return x * y;
+        }
+        static public int Div(int x, int y)
+        {
+            if (y == 0)
+                return 0;
+            return x / y;
+        }
+    }
+        }
+
